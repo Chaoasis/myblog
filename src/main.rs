4d635baee27fa526acfea865d992195a0f3dfa41
chao <chaoasis5597@gmail.com>
2022-09-27 21:54:36 +0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use log::info;
+
+fn init_logger() {
+    use chrono::Local;
+    use std::io::Write;
+
+    let env = env_logger::Env::default()
+        .filter_or(env_logger::DEFAULT_FILTER_ENV, "info");
+    // 设置日志打印格式
+    env_logger::Builder::from_env(env)
+        .format(|buf, record| {
+            writeln!(
+                buf,
+                "{} {} [{}] {}",
+                Local::now().format("%Y-%m-%d %H:%M:%S"),
+                record.level(),
+                record.module_path().unwrap_or("<unnamed>"),
+                &record.args()
+            )
+        })
+        .init();
+    info!("env_logger initialized.");
+}
+
+fn main() {
+    init_logger();
+    info!("hello world");
+}
